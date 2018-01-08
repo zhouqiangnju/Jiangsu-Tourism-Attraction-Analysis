c53dec09f88c33962549e190a20ad9969f9414fc
zhouqiangnju <zhouqiangnju@163.com>
2018-01-08 22:25:18 +0800

    modify

--- get jingqu location with amap api.R
+++ get jingqu location with amap api.R
@@ -7,32 +7,17 @@ library('reshape2')
 library("dplyr")
 library('plyr')
 library('sf')
-library('Rgctc2')
+library('Rgctc')
+library(maptools)
+library(rgdal)
 #data input
-<<<<<<< HEAD
-jqdata<-read.csv("C:/Users/zhouq/Documents/GitHub/Jiangsu-Tourism-Attraction-Analysis/JVTnew.csv",stringsAsFactors = FALSE)[,-1]
-jqname<-as.character(unique(jqdata$Name))
-jqinfo<-jqdata[,c(2,6)]
-jqchengshi<-as.data.frame(table(jqinfo))
-jqchengshi<-jqchengshi[which(jqchengshi$Freq>0),][,-3]
-jqchengshi$Name<-as.character(jqchengshi$Name)
-
-for(i in 1:length(jqchengshi$Name)){
-  if(nchar(jqchengshi$Name[i])<=5)
-    jqchengshi$sname[i]<-jqchengshi$Name[i]
-  else
-    jqchengshi$sname[i]<-sub(jqchengshi$City[i],"",jqchengshi$Name[i])
-}
-sname<-jqchengshi$sname
-sname<-data.frame(jqname,sname)
-failnames<-c("大丰知青纪念馆","无锡江苏学政文化旅游区")
-=======
-
-jqdata<-read.csv("F:/Administrator/Documents/GitHub/Jiangsu-Tourism-Attraction-Analysis/JVTnew.csv",stringsAsFactors = FALSE)[,-1]
+homefile<-'C:/Users/zhouq/Documents/GitHub/Jiangsu-Tourism-Attraction-Analysis/JVTnew.csv'
+workfile<-"F:/Administrator/Documents/GitHub/Jiangsu-Tourism-Attraction-Analysis/JVTnew.csv"
+jqdata<-read.csv(homefile,stringsAsFactors = FALSE)[,-1]
 jqname<-as.character(unique(jqdata$Name))
 
 testnames<-c('大丰知青纪念馆','无锡江苏学政文化旅游区')
->>>>>>> 9826886ca3f975eb44055676f10e41afee897412
+realname<-c('大丰上海知青纪念馆','江阴江苏学政文化旅游区')
 GetJD <- function(address){
   url = "http://restapi.amap.com/v3/geocode/geo"
   header  <- c("User-Agent"="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36")
@@ -53,7 +38,7 @@ GetJD <- function(address){
       addinfo<-rbind(addinfo,geoinfo)
     },error = function(e){
       cat(sprintf("任务【%s】处理失败!",i),sep = "/n")
-      addfail<-c(rep("NA",length(names(addinfo))-1),i)
+      addfail<-c(rep("NA",8),i)
       addinfo <- rbind(addinfo,addfail)
     })
     Sys.sleep(runif(1))
@@ -61,13 +46,12 @@ GetJD <- function(address){
   }
   print("所有数据全部抓取完毕!!!")
   return(addinfo)
-
 }
-<<<<<<< HEAD
-myresult<-GetJD(sname$jqname)
-failresult<-GetJD(failnames)
-finalresult<-rbind(myresult,failresult)
-sresult<-GetJD(sname)
+
+myresult<-GetJD(jqname)
+failresult<-GetJD(testnames)
+jqgeo<-rbind(myresult,failresult)
+jqgeo$Name[c(230,231)]<-realname
 #coordination transform
 myresult2<-myresult
 x_PI <- 3.14159265358979324 * 3000.0 / 180.0
@@ -77,23 +61,20 @@ z<-sqrt(x^2+y^2)-0.00002*sin(y*x_PI)
 theta<-atan2(y,x)-0.000003 * cos(x * x_PI)
 myresult2$lng<-z*cos(theta)
 myresult2$lat<-z*sin(theta)
-=======
 
-myresult<-GetJD(jqname)
-failresult<-GetJD(testnames)
-jqgeo<-rbind(myresult,failresult)
+
 
 #coordination transform(GCJ02-WGS84)
 jqgeo$lng_wgs84<-gcj02_wgs84_lng(jqgeo$lng,jqgeo$lat)
 jqgeo$lat_wgs84<-gcj02_wgs84_lat(jqgeo$lng,jqgeo$lat)
->>>>>>> 9826886ca3f975eb44055676f10e41afee897412
+
 
 #correction
 jqinfo<-jqdata[,c(2,6)]
 jqchengshi<-as.data.frame(table(jqinfo))
 jqchengshi<-jqchengshi[which(jqchengshi$Freq>0),][,-3]
 
-correction<-join(finalresult,jqchengshi,by='Name')
+correction<-join(jqgeo,jqchengshi,by='Name')
 correction$city<-sub('市',"",correction$city)
 correction$match<-correction$city==correction$City
 
@@ -109,28 +90,19 @@ p <- leaflet() %>%
     group="Road Map"
   ) %>% 
   setView(118.788815,32.020729, zoom = 10)%>%
-  addMarkers(finalresult,lng=as.numeric(finalresult$lng),lat=as.numeric(finalresult$lat),popup=finalresult$Name)
+  addMarkers(jqgeo,lng=as.numeric(jqgeo$lng),lat=as.numeric(jqgeo$lat),popup=jqgeo$Name)
 p
 #format and output
-<<<<<<< HEAD
-finalresult$lng<-as.numeric(finalresult$lng)
-finalresult$lat<-as.numeric(finalresult$lat)
-finalresult<-as.data.frame(finalresult)
-finalresult$geo<-list()
 
-for(i in 1:length(finalresult$citycode)){
-  finalresult$geo[[i]]<-st_point(c(finalresult$lng[i],finalresult$lat[i]))
-=======
 
-correctinfo<-as.data.frame(correctinfo)
-correctinfo$geo<-list()
-
-for(i in 1:length(correctinfo$citycode)){
-  geo[[i]]<-st_point(c(correctinfo$lng[i],correctinfo$lat[i]))
->>>>>>> 9826886ca3f975eb44055676f10e41afee897412
+for(i in 1:length(jqgeo$citycode)){
+  jqgeo$geo[[i]]<-st_point(c(jqgeo$lng[i],jqgeo$lat[i]))
 }
-finalresult$geo<-st_sfc(finalresult$geo,crs = 4326)
 
-finalresult<-st_sf(finalresult)
-finalresult
-st_write(finalresult,"co.shp")
+jqgeo$geo<-st_sfc(jqgeo$geo,crs = 4326)
+jqgeo<-st_sf(jqgeo)
+jqgeo$district<-as.character(jqgeo$district)
+jqgeo<-as(jqgeo,"Spatial")
+st_write(jqgeo,"coord.shp")
+writeOGR(jqgeo,'coord',driver="ESRI Shapefile")
+writeSpatialShape(jqgeo,"coord.shp")
